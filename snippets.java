533897b2d22c538f7fda8cfcf6e22c5f79a2b8f4
Marconi Lanna <marconi.lanna@gmail.com>
2012-03-01 15:09:32 -0500

    Added double-checked locking, thread-safe lazy initialization Java sample

---
+++ snippets.java
@@ -0,0 +1,17 @@
+/*
+ * Double-checked locking, thread-safe lazy initialization
+ */
+
+class Foo {
+	private volatile Bar bar = null;
+	public Bar getBar() {
+		if (bar == null) {
+			synchronized (this) {
+				if (bar == null) {
+					bar = new Bar();
+				}
+			}
+		}
+		return bar;
+	}
+}
