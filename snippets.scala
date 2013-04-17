f3e4888c7d0cc49545eacf65b23b0817977c5f9c
Marconi Lanna <marconi.lanna@gmail.com>
2013-04-17 12:26:06 -0400

    Scala snippets

--- snippets.scala
+++ snippets.scala
@@ -1,30 +1,18 @@
 /*
- * Bidirectional conversions between corresponding Scala and Java collections.
- * For a list of supported conversions, see:
- * http://www.scala-lang.org/api/current/scala/collection/JavaConversions$.html
- * http://www.scala-lang.org/api/current/scala/collection/JavaConverters$.html
+ * Main method
  */
 
-// Implicit, automatic conversions
-import scala.collection.JavaConversions._
+// Java style
+object Echo {
+	def main(args: Array[String]) {
+		for (arg <- args) println(arg)
+	}
+}
 
-// asScala, asJava, asJavaCollection, asJavaEnumeration, asJavaDictionary
-import scala.collection.JavaConverters._
-
-
-
-/*
- * Mutable collections.
- * Do not import a mutable collection directly. Import the "mutable" package
- * instead and use the "mutable." prefix to denote mutability explicitly.
- */
-
-import scala.collection.mutable
-val set = mutable.Set(1, 2, 3)
-
-// Counter-example:
-import scala.collection.mutable.Set
-val set = Set(1, 2, 3) // ambiguous for the inattentive reader
+// Alternative
+object Echo extends App {
+	for (arg <- args) println(arg)
+}
 
 
 
@@ -44,14 +32,42 @@ import scala.reflect.BeanProperty
 
 
 /*
- * Main method
+ * Bidirectional conversions between corresponding Scala and Java collections.
+ * For a list of supported conversions, see:
+ * http://www.scala-lang.org/api/current/scala/collection/JavaConversions$.html
+ * http://www.scala-lang.org/api/current/scala/collection/JavaConverters$.html
  */
 
-object Echo {
-	def main(args: Array[String]) {
-		for (arg <- args) println(arg)
-	}
-}
+// Implicit, automatic conversions
+import scala.collection.JavaConversions._
+
+// asScala, asJava, asJavaCollection, asJavaEnumeration, asJavaDictionary
+import scala.collection.JavaConverters._
+
+
+
+/*
+ * Transparently use Java collections as if they were Scala collections
+ */
+
+import scala.collection.JavaConversions.mapAsScalaMap
+var map: scala.collection.mutable.Map[String, String] = new java.util.HashMap[String, String]
+map += "foo" -> "bar"
+assert(map("foo") == "bar")
+
+
+/*
+ * Mutable collections.
+ * Do not import a mutable collection directly. Import the "mutable" package
+ * instead and use the "mutable." prefix to denote mutability explicitly.
+ */
+
+import scala.collection.mutable
+val set = mutable.Set(1, 2, 3)
+
+// Counter-example
+import scala.collection.mutable.Set
+val set = Set(1, 2, 3) // Too risky for the inattentive reader
 
 
 
@@ -59,13 +75,66 @@ object Echo {
  * Collection initialization
  */
 
-val map = Map("one" -> 1, "two" -> 2, "three" ->3)
+val map = Map("one" -> 1, "two" -> 2, "three" -> 3)
 
 val list = List(1, 2, 3)
-val list = Seq(1, 2, 3) // same as above
-val list = 1 :: 2 :: 3 :: Nil // same as above
+val list = Seq(1, 2, 3) // Same as above
+val list = 1 :: 2 :: 3 :: Nil // Same as above
 
 
+
+/*
+ * Initialization with pattern matching
+ */
+
+val tuple = ("foo", 1, 0)
+val (x, y, z) = tuple
+assert((x, y, z) == ("foo", 1, 0))
+
+val option = Some("foo")
+val Some(foo) = option
+assert(foo == "foo")
+
+case class Foo(x: String, y: Int)
+val foo = Foo("foo", 1)
+val Foo(a, b) = foo
+assert((a, b) == ("foo", 1))
+
+val list = List(1, 2, 3, 4, 5, 6)
+val x :: xs = list
+assert((x, xs) == (1, List(2, 3, 4, 5, 6)))
+// Same as above
+val List(x, xs@_*) = list
+assert((x, xs) == (1, List(2, 3, 4, 5, 6)))
+// Skipping elements
+val _ :: a :: b :: _ :: xs = list
+assert((a, b, xs) == (2, 3, List(5, 6)))
+// Works with other collections, too
+val vector = Vector(1, 2, 3, 4, 5, 6)
+val Vector(_, a, b, _, xs@_*) = vector
+assert((a, b, xs) == (2, 3, Vector(5, 6)))
+
+
+
+/*
+ * Seq to variable length argument list
+ */
+
+def foo(args: Int*) = args.foreach{println(_)}
+foo(list:_*)
+
+
+
+/*
+ * Catching exceptions
+ */
+
+try {
+	// ...
+} catch {
+	case e: Exception => // ...
+	case _: Throwable => // Catches all
+}
 
 /*
  * Flexible casting.
@@ -76,7 +145,7 @@ val list = 1 :: 2 :: 3 :: Nil // same as above
  */
 
 e match {
-	case a: Any => a
+	case a: AnyRef => a
 	case _ => throw new ClassCastException
 }
 
@@ -94,6 +163,34 @@ import System.{currentTimeMillis => now}
  * Regular expression extraction
  */
 
-val regex = "(.)(.)(.)".r  // creates a scala.util.matching.Regex object
-val regex(a, b, c) = "xyz" // matches and extracts regex against "xyz"
+val regex = """(.)(.)(.)""".r  // Creates a scala.util.matching.Regex object
+val regex(a, b, c) = "xyz" // Matches and extracts regex against "xyz"
 assert((a, b, c) == ("x", "y", "z"))
+
+"xyz" match {
+	case regex(a, b, c) => // Match found
+	case _ => // No match
+}
+
+
+
+/*
+ * Extractors
+ */
+
+object Twice {
+	def unapply(x: Int) = if (x % 2 == 0) Some(x/2) else None
+}
+val Twice(i) = 20
+assert(i == 10)
+val Twice(j) = 15 // Throws MatchError
+
+// Works with instances, too
+class Foo(i: Int) {
+	def unapply(x: Int) = if (x == i) Some(Some(x)) else Some(None)
+}
+val foo = new Foo(10)
+val foo(a) = 10
+assert(a == Some(10))
+val foo(b) = 12
+assert(b == None)
